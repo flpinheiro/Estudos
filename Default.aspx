cc58aee1648193d53b5db3e28e3e374f888a2aad
Felipe Luís Pinheiro <37678422+flpinheiro@users.noreply.github.com>
2019-08-30 11:44:35 -0300

    default

---
+++ Default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication2.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="Progrma desenvolvido como forma de avaliação de entrevista de emprego para a Alcoweb" />
+    <meta name="author" content="Felipe Luis Pinheiro" />
+    <title>Tabuada</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"  type="text/css" />
+    <style>
+        table {
+            max-width:200px;
+            width:200px;
+            margin:15px;
+            padding:10px;
+        }
+        .tabuada {
+            margin-top:40px;
+        }
+    </style>
+</head>
+<body class="container">
+    <h1>Tabuada</h1>
+    <form id="form1" runat="server">
+        <div>
+            <p>Quantas Tabuada deseja gerar?</p> 
+            <asp:TextBox runat="server" ID="Max" />
+            <asp:Button runat="server" ID="GreetButton" Text="Gerar Tabuadas" />
+            <br />
+            <asp:Label runat="server" ID="Tabuada" CssClass="tabuada"></asp:Label>
+            <br />
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int maximun;
+            string ret = "";
+
+            try
+            {
+                maximun = int.Parse(Max.Text);
+            }
+            catch (FormatException)
+            {
+                maximun = 0;
+            }
+            catch (ArgumentNullException)
+            {
+                maximun = 0;
+            }
+            catch (OverflowException)
+            {
+                maximun = 0;
+            }
+
+            if (maximun > 0)
+            {
+                int i = 1;
+                while (i <= maximun)
+                {
+                    ret += "<div class='row'>";
+                    ret += "<div class='col-2'>";
+                    ret += "</div>";
+                    for (int j = 0; j < 5 && i <= maximun; j++, i++)
+                    {
+                        ret += "<div class='col-2'>";
+                        if (i % 2 == 0)
+                        {
+                            ret += "<table class='table table-hover table-bordered' > ";
+                        }
+                        else
+                        {
+                            ret += "<table class='table table-hover table-primary table-bordered' > ";
+                        }
+
+                        for (int k = 0; k < 11; k++)
+                        {
+                            ret += "<tr>";
+                            ret += $"<td>{i} X {k} = {i * k}</td>";
+                            ret += "</tr>";
+                        }
+                        ret += "</table>";
+                        ret += "</div>";
+                    }
+                    ret += "</div>";
+                }
+            }
+            Tabuada.Text = ret;
+        }
+    }
+}
